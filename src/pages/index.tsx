1a0eb9e653f61943fd9146e2ffe18ee0d2f3eb22
Jason Wang <xoutdragon@gmail.com>
2024-02-28 12:06:11 -0500

    Big

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
-import { UserButton } from '@clerk/nextjs';
-import Image from 'next/image';
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
-import { LoginForm } from './components/LoginForm';
+import { LoginForm } from "./components/LoginForm";
 
 export default function Home() {
-	return (
-		<>
-			<main className='grid grid-cols-2 h-full'>
-				<Image
-					src='/hero.gif'
-					width={100}
-					height={100}
-					alt='hero'
-					objectFit='cover'
-				/>
-				<LoginForm />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main className="grid grid-cols-2 h-full">
+        <div className="relative w-full h-full">
+          <Image src="/hero.gif" layout="fill" alt="hero" objectFit="cover" />
+        </div>
+        <LoginForm />
+      </main>
+    </>
+  );
 }
